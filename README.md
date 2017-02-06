f76c9682ea1aaaea87d2d57c74c865913f0c3804
Paolo Carrara <paolocarraratux@gmail.com>
2017-02-06 01:49:31 -0200

    Realiza a integração com o banco de dados. Cria uma rota para a criação de Chests. Complementa o README.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # pandaAPI
+
+Todo git pull
+  npm install
+
+Definir as variáveis de sessão (inclua essas linhas no arquivo ~/.bashrc):
+  export PANDA_DB_PASSWORD="your_db_password"
+  export PANDA_DB_USERNAME="your_db_username"
+  export PANDA_DB_NAME="your_db_name"
+
+Para usa-las pode ser preciso executar o seu .bashrc
+  . ~/.bashrc ou source ~/.bashrc
+
+Para rodar a aplicação:
+  node app.js
+
+Para rodar a aplicação utilizando o supervisor(https://github.com/petruisfan/node-supervisor):
+  supervisor app.js
+
+Para testar o post:
+  Ex.: curl -X POST --data "name=Mochila&description=Minha mochila da faculdade." http://localhost:3000/chests
+
+Para testar o get:
+  Ex.: curl http://localhost:3000/chests
+
+Sobre o banco de dados.
+  Estamos utilizando o Sequelize(https://www.npmjs.com/package/sequelize), este pacote do node é uma ORM para Postgres, MySQL, etc ou seja, só precisamos criar o banco de dados e o resto será criado automaticamente quando for necessário para a aplicação usar.
+
+
+Ref.:
+  https://www.digitalocean.com/community/tutorials/how-to-create-a-new-user-and-grant-permissions-in-mysql
+  http://stackoverflow.com/questions/14417592/node-js-difference-between-req-query-and-req-params
+  http://stackoverflow.com/questions/22100592/curl-with-nodejs-express-post-data
+  http://askubuntu.com/questions/58814/how-do-i-add-environment-variables
+  http://superuser.com/questions/46139/what-does-source-do
--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var Sequelize = require('sequelize');
+var mysql = require('mysql');
 
+/**
+* @todo Colocar em um arquivo separado.
+*/
+var sequelize = new Sequelize (process.env.PANDA_DB_NAME, process.env.PANDA_DB_USERNAME, process.env.PANDA_DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+
+/**
+* @todo: Colocar em um novo arquivo.
+*/
+var Chest = sequelize.define('chest', {
+  id: {
+    type: Sequelize.BIGINT,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING
+  }
+});
+
+/**
+* Middlewares.
+*/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({exteded: true}));
+
+/**
+* @todo Remover essa rota.
+*/
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -11,17 +59,34 @@ app.use(function (req, res, next){
 	next();
 });
 
+/**
+* Pega todos os baús.
+*/
 app.get('/chests', function (req, res) {
-	var chests = [
-    {
-  		id: 1,
-  		name: 'Carteira',
-  		description: 'Meu precioso dinheiro!'
-  	}
-  ]
-	res.json(chests);
+  Chest.findAll().then(function (chests) {
+    res.json(chests);
+  },function (err) {
+    res.status(500).json({error: err});
+  })
 });
 
+/**
+* Insere um baú.
+*/
+app.post('/chests', function (req, res) {
+  Chest.create({
+    name: req.body.name,
+    description: req.body.description
+  }).then(function (chest) {
+    res.status(200).json();
+  }, function (err) {
+    res.status(500).json({error: err});
+  })
+});
+
+/**
+* Inicia o servidor.
+*/
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
